d79d59b96da1fbe500fe6e60536d959fc20c262b
Anoosh K <55269680+Anoosh-p73@users.noreply.github.com>
2024-03-06 13:40:45 -0800

    BugFixes

--- Assets/Scripts/Analytics.cs
+++ Assets/Scripts/Analytics.cs
@@ -7,14 +7,14 @@ using UnityEngine.Networking;
 public class Analytics : MonoBehaviour
 {
     string URL = "https://docs.google.com/forms/d/e/1FAIpQLSfYqvkOq-JMVyZKIsPyyGUV7eEUoOIuyndzBir0wxA1opyAQw/formResponse";
-
+    // https://docs.google.com/forms/u/2/d/e/1FAIpQLSfYqvkOq-JMVyZKIsPyyGUV7eEUoOIuyndzBir0wxA1opyAQw/formResponse?usp=pp_url&entry.760059763=DebugTest
     [Button]
     void DebugPost() {
-        StartCoroutine(GetRequest("https://docs.google.com/forms/u/2/d/e/1FAIpQLSfYqvkOq-JMVyZKIsPyyGUV7eEUoOIuyndzBir0wxA1opyAQw/formResponse?usp=pp_url&entry.760059763=DebugTest"));
+        StartCoroutine(Post("DebugTest"));
     }
 
     public void Send(string s) {
-        StartCoroutine(GetRequest($"https://docs.google.com/forms/u/2/d/e/1FAIpQLSfYqvkOq-JMVyZKIsPyyGUV7eEUoOIuyndzBir0wxA1opyAQw/formResponse?usp=pp_url&entry.760059763={s}"));
+        StartCoroutine(Post($"{s}"));
     }
 
     IEnumerator Post(string Analytic) {
