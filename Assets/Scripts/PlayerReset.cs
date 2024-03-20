0a58677f54484503cb94169fce15c0e9f9266b1e
Tri Nguyen <khactrist99@gmail.com>
2024-03-20 16:28:40 -0700

    reset whole scene instead of position

--- Assets/Scripts/PlayerReset.cs
+++ Assets/Scripts/PlayerReset.cs
@@ -16,7 +16,9 @@ public class PlayerReset : MonoBehaviour
 
     private void Update() {
         if (player.transform.position.y < resetHeight) {
-            player.transform.position = respawnPoint[0].position;
+             // Reload the current scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // player.transform.position = respawnPoint[0].position;
             // if (GameManager.Instance.CurrentRotation == 0) {
             //     player.transform.position = respawnPoint[0].position;
             // } else if (GameManager.Instance.CurrentRotation == 90) {
@@ -24,7 +26,7 @@ public class PlayerReset : MonoBehaviour
             //     player.transform.position = respawnPoint[1].position;
             // }
             
-            player.GetComponent<PlayerMovement>().Reset();
+            // player.GetComponent<PlayerMovement>().Reset();
 
             // analytics.Send($"{SceneManager.GetActiveScene().name}: {GameManager.Instance.CurrentPlatform}");
 
