29ac3c258cfb75f520d856677602eae11e61e0ff
Tri Nguyen <khactrist99@gmail.com>
2024-04-02 11:36:06 -0700

    hotfix checkpoint

--- Assets/Scripts/CheckpointScript.cs
+++ Assets/Scripts/CheckpointScript.cs
@@ -22,7 +22,47 @@ public class CheckpointScript : MonoBehaviour
         gameObject.SetActive(false);
         if (checkpointText != null)
             checkpointText.gameObject.SetActive(false);
-        // TODO: rename GameManager.GameState.MirrorLevel, MirrorLevel works as a checkpoint
+    
         GameManager.Instance.checkpoint = true; 
+        GameManager.Instance.checkpointZRotation = GameManager.Instance.currentZRotation;
+        StoreDeactivatedGroundObjects();
+    }
+
+    void StoreDeactivatedGroundObjects()
+    {
+        // // Find the LevelParent GameObject
+        // GameObject horPlatform = GameObject.Find("Hor explode platform");
+
+        // if (horPlatform != null)
+        // {
+        //     // Get the number of children
+        //     int childCount = horPlatform.transform.childCount;
+
+        //     // Loop through all children
+        //     for (int i = 0; i < childCount; i++)
+        //     {
+        //         // Get the child GameObject
+        //         GameObject child = horPlatform.transform.GetChild(i).gameObject;
+
+        //         // Now you can do something with the child GameObject
+        //         // For example, print its name
+        //         Debug.Log(child.name + " " + child.activeInHierarchy);
+        //     }
+        // }
+        // Get all GameObjects
+        GameObject[] allObjects = Resources.FindObjectsOfTypeAll<GameObject>();
+
+        // Loop through all GameObjects
+        foreach (GameObject obj in allObjects)
+        {
+            
+            // If the GameObject has the tag "Ground" and is deactivated
+            if (obj.tag == "Ground" && !obj.activeInHierarchy)
+            {
+                // Add the GameObject to the list
+                GameManager.Instance.deactivatedPlatforms.Add(obj);
+            }
+        }
+        Debug.Log("Deactivated platforms: " + GameManager.Instance.deactivatedPlatforms.Count);
     }
 }
