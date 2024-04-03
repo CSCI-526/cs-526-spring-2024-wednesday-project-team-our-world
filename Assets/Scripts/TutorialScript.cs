3c21b04a23681f788b2c662cabaa3374f5cd0172
Anoosh K <55269680+Anoosh-p73@users.noreply.github.com>
2024-04-03 13:10:59 -0700

    Analytics on level completion

--- Assets/Scripts/TutorialScript.cs
+++ Assets/Scripts/TutorialScript.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour
 {
 
     public GameObject UI;
+    bool lockout = false;
 
     private void OnCollisionEnter(Collision collision) {
         print("Entering Mirror World");
@@ -15,6 +18,37 @@ public class TutorialScript : MonoBehaviour
         UI.SetActive(true);
         Time.timeScale = 0;
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (!lockout) { 
+            lockout = true;
+
+            //Analytics
+            //metric 2
+            string metric2Data = $"{SceneManager.GetActiveScene().name}: {GameManager.Instance.CurrentPlatform}";
+            //metric 4
+            string metric4Data = $"{SceneManager.GetActiveScene().name}: {GameManager.Instance.checkpointUsed}";
+            //metric 1
+            string metric1Data = $"{SceneManager.GetActiveScene().name}:\n";
+            foreach (KeyValuePair<string, float> pair in GameManager.Instance.platformTimes) {
+                metric1Data += $"{pair.Key}: {pair.Value}\n";
+            }
+            //metric 3
+            string metric3Data = $"{SceneManager.GetActiveScene().name}:\n";
+            foreach (KeyValuePair<string, int> pair in GameManager.Instance.platformRotateTimes) {
+                metric3Data += $"{pair.Key}: {pair.Value}\n";
+            }
+
+            analytics.AddAnalyticData(metric1Data, 0);
+            analytics.AddAnalyticData(metric2Data, 1);
+            analytics.AddAnalyticData(metric3Data, 2);
+            analytics.AddAnalyticData(metric4Data, 3);
+            if (!locked) {
+                locked = true;
+                analytics.Send();
+            }
+
+        }
+
     }
 
     public void Proceed() {
