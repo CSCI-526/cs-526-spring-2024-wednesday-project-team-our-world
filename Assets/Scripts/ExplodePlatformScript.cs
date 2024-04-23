379ee5c82e3ccb9b0bdb6c5f21c0f1648a71bf61
Tri Nguyen <khactrist99@gmail.com>
2024-04-22 22:56:00 -0700

    change timeToExplode to 1.5 second and update Gold 3

--- Assets/Scripts/ExplodePlatformScript.cs
+++ Assets/Scripts/ExplodePlatformScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ExplodePlatformScript : MonoBehaviour
 {   
-    private float timeToExplode = 1.25f;
+    private float timeToExplode = 1.5f;
     // Start is called before the first frame update
     Renderer r;
     Collider c;
